b8c1e2202e918e7670438e3ed73e8d110467bca7
MOTHINK <mohamedadelbenmimoun@gmail.com>
2023-02-20 23:03:28 +0100

    Mon Feb 20 11:03:28 PM CET 2023

--- src/view/View1.java
+++ src/view/View1.java
@@ -29,7 +29,7 @@ public class View1 extends javax.swing.JFrame {
         initComponents();
         utils.iconFitInLabel(dockerIcon,DOCKER_ICON_TEXT_NAME);
         this.setTitle(this.TITLE);
-        this.centreWindow(this);
+        this.centreWindowAndShow(this);
         
         dockerController = new DockerController();
         
@@ -37,12 +37,12 @@ public class View1 extends javax.swing.JFrame {
         this.passwordInputWindow(v2);
         this.passwordSettings(v2);
         
-        if(!dockerController.isDockerInstalled){
-            dockerController.installDocker();
+        if(dockerController.isDockerInstalled){
+            this.InstallDockerButton.setVisible(false);
         }
     }
     
-    private void centreWindow(Window frame) {
+    private void centreWindowAndShow(Window frame) {
         frame.pack();
         frame.setLocationRelativeTo(null);
         if(!frame.isVisible()){
@@ -52,18 +52,15 @@ public class View1 extends javax.swing.JFrame {
     
     private void passwordInputWindow(Window frame){
         if(!dockerController.checkFileExists(Filenames.PASSWORD_FILENAME_PATH)){
-            this.centreWindow(frame);
+            this.centreWindowAndShow(frame);
         }
     }
     
     private void passwordSettings(Window v2){
-        ArrayList<String> s = dockerController.checkDockerExistsInOS();
         if(!this.dockerController.checkFileExists(Filenames.PASSWORD_FILENAME_PATH) ||
            !this.dockerController.checkIfSystemPasswordIsCorrect())
         {
             this.passwordInputWindow(v2);
-        } else {
-            this.Prueba.setText(s.get(0));
         }
     }
     
@@ -80,6 +77,8 @@ public class View1 extends javax.swing.JFrame {
         BodyPanel = new javax.swing.JPanel();
         Prueba = new javax.swing.JLabel();
         HeaderPanel = new javax.swing.JPanel();
+        InstallDockerButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -106,15 +105,39 @@ public class View1 extends javax.swing.JFrame {
 
         HeaderPanel.setBackground(new java.awt.Color(0, 102, 153));
 
+        InstallDockerButton.setBackground(new java.awt.Color(0, 153, 51));
+        InstallDockerButton.setFont(new java.awt.Font("Liberation Sans", 1, 14)); // NOI18N
+        InstallDockerButton.setForeground(new java.awt.Color(0, 0, 0));
+        InstallDockerButton.setText("Install Docker");
+        InstallDockerButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                InstallDockerButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Liberation Sans", 1, 14)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 0, 0));
+        jLabel1.setText("Docker is not installed in the system!!");
+
         javax.swing.GroupLayout HeaderPanelLayout = new javax.swing.GroupLayout(HeaderPanel);
         HeaderPanel.setLayout(HeaderPanelLayout);
         HeaderPanelLayout.setHorizontalGroup(
             HeaderPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 922, Short.MAX_VALUE)
+            .addGroup(HeaderPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(HeaderPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(InstallDockerButton, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(654, Short.MAX_VALUE))
         );
         HeaderPanelLayout.setVerticalGroup(
             HeaderPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(HeaderPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(InstallDockerButton)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -145,6 +168,11 @@ public class View1 extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void InstallDockerButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InstallDockerButtonActionPerformed
+        View3 v3 = new View3(utils, dockerController, this);
+        this.centreWindowAndShow(v3);
+    }//GEN-LAST:event_InstallDockerButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -184,7 +212,9 @@ public class View1 extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel BodyPanel;
     private javax.swing.JPanel HeaderPanel;
+    private javax.swing.JButton InstallDockerButton;
     private javax.swing.JLabel Prueba;
     private javax.swing.JLabel dockerIcon;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
